fadc09b8a3beb49d67364a3143031fa67508f59b
Robert F. Dickerson <rfdickerson@gmail.com>
2015-12-20 21:42:11 -0600

    used velocity verlet for integration

--- src/scripts/pendulum.js
+++ src/scripts/pendulum.js
@@ -1,14 +1,15 @@
 
 // Constants
 var g = 9.8;
-var p = new pendulum(130, .5);
-var rp = new rimPendulum(0.2, 100.0, 100.0);
+var p = new pendulum(130, 2.14);
+var rp = new rimPendulum(0.0, 100.0, 200.0);
 var lastUpdate = 0.0;
 var EPSILON = 0.01;
 
 var g = newtonRaphson(
-  function(x) { return x*x - 5*x + 4; },
-  2);
+  function(x) { return x*x - 5*x - 4; },
+  0);
+
 console.log(g);
 
 function pendulum(length, angle) {
@@ -24,7 +25,7 @@ function rimPendulum(omega, radius, length) {
   this.rotation = 0.0;
   this.radius = radius;
   this.length = length;
-  this.angle = 0.0;
+  this.angle = 3.14;
   this.angleV = 0.0;
 }
 
@@ -45,23 +46,32 @@ function newtonRaphson(f, guess) {
   if (diff <= EPSILON) {
     return newguess;
   } else {
-    newtonRaphson(f, newguess);
+    return newtonRaphson(f, newguess);
   }
   
 }
 
+
+// Approximation of the derivative at x 
 function derivative(x, h, f) {
   return (f(x+h) - f(x)) / h;
 }
 
-function update(dt) {
+function updatePendulum(dt) {
 
   // compute acceleration
   var accel = -g/p.length * Math.sin(p.angle);
 
   // integrate velocity
-  p.velocity = p.velocity + accel * dt;
-    
+  // p.velocity = p.velocity + accel * dt;
+
+  var v = p.velocity;
+  
+  p.velocity = newtonRaphson(
+    function(x) {
+      return x - v - dt * accel;
+    }, 0);
+  
   // integrate position
   p.angle = p.angle + p.velocity * dt;
 }
@@ -74,14 +84,31 @@ function updateRim(dt) {
   var omega = rp.omega;
   var rotation = rp.rotation;
     
-  var accel = a/b * omega*omega * Math.cos(theta - rotation)
-      - g/b * Math.sin(theta - rotation);
+  var accel = a/b * omega*omega
+      * Math.cos(theta - rotation)
+      - g/b * Math.sin(theta);
 
   rp.rotation = rotation - dt * omega;
-        
-  rp.angleV = rp.angleV + dt * accel;
-    
-  rp.angle = rp.angle + dt * rp.angleV;
+
+  // rp.angleV = rp.angleV + dt * accel;
+  var p1 = rp.angle + dt * rp.angleV + 0.5*accel*dt*dt;
+  var v1 = rp.angleV + 0.5*accel*dt;
+
+  var accel2 = a/b * omega*omega
+      * Math.cos(p1 - rotation)
+      - g/b * Math.sin(p1)
+
+  rp.angleV = v1 + 0.5*accel2*dt;
+
+  rp.angle = p1;
+
+  // var v = rp.angleV;
+  // rp.angleV = newtonRaphson(
+  //   function(x) {
+  //     return x - v - dt * accel;
+  //   }, 0);
+  
+ 
     
     
 }
@@ -91,11 +118,10 @@ function drawRimPendulum(ctx) {
     var bx = rp.x + rp.radius*Math.cos(rp.rotation);
     var by = rp.y + rp.radius*Math.sin(rp.rotation);
 
-    var px = bx + rp.length*Math.sin(rp.angle);
-    var py = by + rp.length*Math.cos(rp.angle);
+    var px = bx + rp.length*Math.sin(rp.angle + Math.PI);
+    var py = by + rp.length*Math.cos(rp.angle + Math.PI);
 
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = 'rgba(0,153,255,0.4)';
+   
     ctx.beginPath();
     ctx.arc(rp.x, rp.y, rp.radius, 0, 2*Math.PI);
     ctx.closePath();
@@ -111,6 +137,24 @@ function drawRimPendulum(ctx) {
     
 }
 
+function drawPendulum(ctx) {
+  var offsetx = p.length*Math.sin(p.angle + Math.PI);
+  var offsety = p.length*Math.cos(p.angle + Math.PI);
+
+  var bx = 500;
+  var by = 100;
+    
+  ctx.beginPath();
+  ctx.moveTo(bx,by);
+  ctx.lineTo(bx+offsetx,by+offsety);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.arc(bx + offsetx, by + offsety, 5, 0, 2*Math.PI);
+  ctx.stroke();
+}
+
 // draw function
 function draw() {
 
@@ -123,35 +167,33 @@ function draw() {
   var dt = time.getTime() - lastUpdate;
   lastUpdate = time.getTime();
 
-    dt *= 0.01
+  dt *= 0.1
     
-    update(dt);
-    updateRim(dt);
-    
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+  updatePendulum(dt);
+  updateRim(dt);
+  
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
 
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    
-    ctx.fillStyle = "#FFFF00";
+  ctx.globalCompositeOperation = 'destination-over';
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-    drawRimPendulum(ctx);
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = 'rgba(0,153,255,1.0)';
+  ctx.fillStyle = "#FFFF00";
 
-    var offsetx = p.length*Math.sin(p.angle);
-    var offsety = p.length*Math.cos(p.angle);
+  drawRimPendulum(ctx);
 
-    var bx = 100;
-    var by = 10;
-    
-    ctx.beginPath();
-    ctx.moveTo(bx,by);
-    ctx.lineTo(bx+offsetx,by+offsety);
-    ctx.closePath();
-    ctx.stroke();
-
-    window.requestAnimationFrame(draw);
-    // setTimeout(draw, 5);
+  drawPendulum(ctx);
+  
+  window.requestAnimationFrame(draw);
+  // setTimeout(draw, 5);
     
 }
 
+function onVelocityChanged(slider) {
+
+  var vel = slider.value;
+  rp.omega = vel;
+  
+}
